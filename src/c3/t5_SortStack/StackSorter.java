9c044de8c91a4fc75ce38ad40fdf35a0facd6122
kahneraja <kahneraja@gmail.com>
2017-09-19 19:38:20 -0400

    edit

--- src/c3/t5_SortStack/StackSorter.java
+++ src/c3/t5_SortStack/StackSorter.java
@@ -3,69 +3,26 @@ package c3.t5_SortStack;
 import java.util.Stack;
 
 public class StackSorter {
-    public static Stack sort(Stack<Integer> a) {
-
-
+    public static void sort(Stack<Integer> a) {
         Stack<Integer> b = new Stack();
+        int hold;
 
-        Tuple minMax = findMinMax(a, b);
-
-        while(minMax.min < minMax.max){
-            sort(a,b, minMax);
-        }
-
-        return a;
-    }
-
-    private static void sort(Stack<Integer> a, Stack<Integer> b, Tuple minMax) {
-        int count = 0;
         while(!a.isEmpty()){
-            int v = a.pop();
-            if (v == minMax.min){
-                count++;
+            if (b.isEmpty() || a.peek() >= b.peek()){
+                b.push(a.pop());
             } else {
-                b.push(v);
-            }
-        }
-
-        while(!b.isEmpty()){
-            int v = b.pop();
-            if (v > minMax.min){
-                a.push(v);
-            } else {
-                while(count > 0){
-                    a.push(minMax.min);
-                    count--;
+                hold = a.pop();
+                while(!b.isEmpty() && b.peek() > hold){
+                    a.push(b.pop());
                 }
+                b.push(hold);
             }
-
-            while(count > 0){
-                a.push(minMax.min);
-                count--;
-            }
-        }
-    }
-
-    private static Tuple findMinMax(Stack<Integer> a, Stack<Integer> b) {
-        Integer min = null;
-        Integer max = null;
-
-        while(!a.isEmpty()){
-            int v = a.pop();
-            if (min == null || min > v){
-                min = v;
-            }
-            if (max == null || max < v){
-                max = v;
-            }
-            b.push(v);
         }
 
         while(!b.isEmpty()){
             a.push(b.pop());
         }
-
-        return new Tuple(min, max);
     }
+
 
 }
--- src/c3/t5_SortStack/Tests.java
+++ src/c3/t5_SortStack/Tests.java
@@ -14,8 +14,34 @@ public class Tests {
         s.push(3);
         s.push(1);
         s.push(2);
-        Stack sorted = StackSorter.sort(s);
-        assertEquals(1, sorted.pop());
+        StackSorter.sort(s);
+        assertEquals(1, (int)s.pop());
+        assertEquals(2, (int)s.pop());
+        assertEquals(3, (int)s.pop());
+    }
+
+    @Test
+    public void testB(){
+        Stack<Integer> s = new Stack();
+        s.push(3);
+        s.push(9);
+        s.push(1);
+        s.push(4);
+        s.push(7);
+        s.push(2);
+        s.push(6);
+        s.push(5);
+        s.push(8);
+        StackSorter.sort(s);
+        assertEquals(1, (int)s.pop());
+        assertEquals(2, (int)s.pop());
+        assertEquals(3, (int)s.pop());
+        assertEquals(4, (int)s.pop());
+        assertEquals(5, (int)s.pop());
+        assertEquals(6, (int)s.pop());
+        assertEquals(7, (int)s.pop());
+        assertEquals(8, (int)s.pop());
+        assertEquals(9, (int)s.pop());
     }
 
 }
